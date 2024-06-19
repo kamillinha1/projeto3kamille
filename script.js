2910cee8d4bf444d73820f9fafc75baeffbbf495
kamillinha1 <108131547+kamillinha1@users.noreply.github.com>
2024-06-19 11:07:18 -0300

    Add files via upload

--- script.js
+++ script.js
@@ -3,3 +3,10 @@ const caixaPerguntas = document.querySelector(".caixa-perguntas");
 const caixaAlternativas = document.querySelector(".caixa-alternativas");
 const caixaResultado = document.querySelector(".caixa-resultado");
 const textoResultado = document.querySelector(".texto-resultado");
+
+const perguntas = [
+   enunciado: "Pergunta 1",
+   alternativas: ["alternativa 1",]
+   
+   "Pergunta 2"
+]
